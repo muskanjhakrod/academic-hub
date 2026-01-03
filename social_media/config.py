349389b9062204c7ab1a800a99d52fba4896ed47
Muskan <jhakrodmuskan@gmail.com>
2026-01-03 18:35:56 +0530

    Delete social_media directory

--- social_media/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-change-this"
-    MONGO_URI = os.environ.get("MONGO_URI") or "mongodb://localhost:27017/SocialMediaDB"
